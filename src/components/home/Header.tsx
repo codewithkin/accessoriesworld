a425eb38e1b0478731ac03d8065b4ab7630a559d
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-02 11:14:59 +0200

    [ADD]: Responsive header component

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,6 +1,11 @@
 "use client";
 import { useEffect, useState } from "react";
 import { Badge } from "../ui/badge";
+import { Skeleton } from "../ui/skeleton";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { ShoppingCart } from "lucide-react";
+import { motion } from "framer-motion";
 
 function Header() {
   const [header, setHeader] = useState<null | string>(null);
@@ -43,15 +48,47 @@ function Header() {
   });
 
   return (
-    <header className="min-h-screen w-full flex flex-col justify-center items-center mx-auto">
+    <motion.header className="min-h-screen w-full flex flex-col justify-center items-center mx-auto p-4 md:p-0">
       <Badge
         className="px-4 py-2 flex items-center gap-4 bg-purple-500 text-white rounded-full"
         variant="outline"
       >
         <span>Say goodbye to mystery cables and earphones that ghost you</span>
       </Badge>
-      <h2>Header</h2>
-    </header>
+      <article className="flex flex-col gap-2 justify-center items-center text-center">
+        <h2 className="text-4xl md:text-6xl font-semibold">
+          {header ? header : <Skeleton className="bg-slate-400 w-full h-40" />}
+        </h2>
+        <p className="text-slate-600 md:max-w-4xl text-center">
+          From earphones that won’t bail on you to charging cables that actually
+          last—we’ve got the good stuff. No more ‘low battery’ panic or tangled
+          messes, just quality accessories that keep up with you
+        </p>
+      </article>
+
+      {/* Call to Actions */}
+      <article className="flex flex-col md:flex-row gap-2 items-center my-4 w-full md:w-fit">
+        <Button
+          className="w-full md:w-auto flex gap-2 items-center"
+          variant="default"
+          asChild
+        >
+          <Link href="/products">
+            <ShoppingCart size={24} />
+            <span>Shop Now</span>
+          </Link>
+        </Button>
+        <Button
+          className="w-full md:w-auto flex gap-2 items-center"
+          variant="secondary"
+          asChild
+        >
+          <Link href="/products">
+            <span>Browse</span>
+          </Link>
+        </Button>
+      </article>
+    </motion.header>
   );
 }
 
