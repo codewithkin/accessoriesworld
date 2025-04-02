02e3f1d434187a32beadc4bbbb66e19e8c7ea75a
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-02 14:18:39 +0200

    [ADD]: Why choose us section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/home/Header";
 import ProductsSwiper from "@/components/home/ProductsSwiper";
+import WhyChooseUs from "@/components/home/WhyChooseUs";
 
 export default function Home() {
   return (
     <>
       <Header />
       <ProductsSwiper />
+      <WhyChooseUs />
     </>
   );
 }
---
+++ src/components/home/WhyChooseUs.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function WhyChooseUs() {
+  return (
+    <section>
+        <article className="flex flex-col gap-2 items-center text-cenmter">
+            <h3 className='text-3xl font-semibold'>Because your gadgets deserve the best</h3>
+            <p className='text-slate-500'>At Accessories World, we know good accessories make life easier. A strong charging cable, clear-sounding earphones, or a long-lasting battery—these small things matter. That’s why we only sell quality products at good prices</p>
+        </article>
+    </section>
+  )
+}
+
+export default WhyChooseUs
