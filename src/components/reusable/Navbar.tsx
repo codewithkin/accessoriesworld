4dcdcdb508dcd58b355013fc96382364d2e9ff2f
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-02 09:43:15 +0200

    Minor updates

---
+++ src/components/reusable/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+        <h1 className='text-2xl text-red-500 font-semibold'>Accessories World</h1>
+
+        <article className="flex gap-4 items-center">
+
+        </article>
+
+        
+    </nav>
+  )
+}
+
+export default Navbar
