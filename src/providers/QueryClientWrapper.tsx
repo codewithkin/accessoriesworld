4bd5340bbf6448113a3ffea106b94af6334aa448
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-02 10:09:26 +0200

    Code format

--- src/providers/QueryClientWrapper.tsx
+++ src/providers/QueryClientWrapper.tsx
@@ -1,15 +1,13 @@
 "use client";
-import { ReactNode } from "react"
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient();
 
-function QueryClientWrapper({children}: Readonly<{children: ReactNode}>) {
+function QueryClientWrapper({ children }: Readonly<{ children: ReactNode }>) {
   return (
-    <QueryClientProvider client={queryClient}>
-        {children}
-    </QueryClientProvider>
-  )
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
 }
 
-export default QueryClientWrapper
+export default QueryClientWrapper;
