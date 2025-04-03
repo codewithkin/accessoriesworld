780aa7b1866eb264da67c3814ece8f363c350fac
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-03 14:57:20 +0200

    [ADD]: Implement GET endpoint to retrieve specific receipt by ID

--- src/app/api/receipt/route.ts
+++ src/app/api/receipt/route.ts
@@ -38,3 +38,35 @@ export async function POST(request: NextRequest) {
     );
   }
 }
+
+// Get a specific receipt
+export async function GET (request: NextRequest) {
+  try {
+    // Get the receipt's id
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get("id");
+
+    if(!id) {
+      console.log("Receipt id was not provided")
+
+      return NextResponse.json({
+        message: "Receipt id was not provided"
+      }, {status: 500})
+    }
+
+    // Get the receipt's data
+    const receipt = await prisma.receipt.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    return NextResponse.json(receipt);
+  } catch (e) {
+    console.log("An error occured while fetching receipt's data: ", e);
+
+    return NextResponse.json({
+      message: "An error occured"
+    }, {status: 500})
+  }
+}
